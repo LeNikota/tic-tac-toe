be7e3290a8c36b8b409586804944d6594df09072
Nikita <nikita@oden.com>
2023-01-16 02:08:23 +0300

    Make the gameBoardModule workable

--- script.js
+++ script.js
@@ -1,38 +1,54 @@
 const gameBoardModule = (() => {
-  const init = () => {
-    createBoard();
-    createPlayers();
-  };
-
-  const resetBoard = () => {
-    const boardArr = [
-      ['', '', ''],
-      ['', '', ''],
-      ['', '', ''],
-    ];
-  };
-
-  const createBoard = () => {
-    const CELLS = document.querySelectorAll('.cell');
-    CELLS.forEach((e) => {
-      e.addEventListener('click', handleCellClick, { once: true });
-    });
-  };
+  const createBoard = () => ['', '', '', '', '', '', '', '', ''];
 
   const createPlayers = () => {
     const playerIdOne = document.querySelector('#player-one');
     const playerIdTwo = document.querySelector('#player-two');
-    const playerOne = playerFactory('Player 1', 'x', playerIdOne);
-    const playerTwo = playerFactory('Player 2', 'o', playerIdTwo);
-    return { playerOne, playerTwo };
+    const one = playerFactory('Player 1', 'x', playerIdOne);
+    const two = playerFactory('Player 2', 'o', playerIdTwo);
+    let turn = one;
+
+    const changePlayersTurn = () => {
+      turn = (turn === one) ? two : one;
+    };
+    const getCurrentPlayerMark = () => {
+      const mark = turn.getSide();
+      changePlayersTurn();
+      return mark;
+    };
+
+    return { one, two, getCurrentPlayerMark };
+  };
+
+  const findCell = (element) => {
+    const cellsArr = Array.from(document.querySelectorAll('.cell'));
+    return cellsArr.indexOf(element);
+  };
+
+  const fillCell = (element) => {
+    const index = findCell(element);
+    boardArr[index] = player.getCurrentPlayerMark();
+  };
+
+  const player = createPlayers();
+  const boardArr = createBoard();
+  return { fillCell, boardArr };
+})();
+
+const displayModule = (() => {
+  const createBoard = () => {
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach((e) => {
+      e.addEventListener('click', handleCellClick, { once: true });
+    });
   };
 
   const handleCellClick = (e) => {
-
+    const index = e.target;
+    gameBoardModule.fillCell(index);
   };
 
-  init();
-
+  createBoard();
   return {};
 })();
 
@@ -40,5 +56,8 @@ function playerFactory(name, side, id) {
   const playerName = name;
   const playerSide = side;
   const playerId = id;
-  return { playerName, playerSide, playerId };
+  const getName = () => playerName;
+  const getSide = () => playerSide;
+  const getId = () => playerId;
+  return { getName, getSide, getId };
 }
